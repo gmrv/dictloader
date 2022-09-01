e57c1e85b2b82b64973af0c1423244ee225c77e0
Shamil Gumerov <shamil.gumerov@gmail.com>
2022-09-01 20:23:45 +0300

    feat[all]: Source v03 support

--- tables/subdivision.py
+++ tables/subdivision.py
@@ -11,13 +11,13 @@ class Subdivision:
     def __init__(self):
 
         self.tablename = 'subdivision'
-        self.filepath_src = 'src/Подразделения v01.xlsx'
+        self.filepath_src = 'src/Подразделения v03.xlsx'
         self.sheet_name = 'Подразделения'
         self.filepath_out = 'out/subdivision.sql'
         self.filepath_out_closure = 'out/subdivision_closure.sql'
 
         self.log = Logger(self.tablename).log
-        self.df = pd.read_excel(self.filepath_src, sheet_name=self.sheet_name, header=None)
+        self.df = pd.read_excel(self.filepath_src, sheet_name=self.sheet_name)
         self.df = self.df.reset_index()
 
         self.log.info(f'Successfully read {len(self.df)} lines')
@@ -31,21 +31,25 @@ class Subdivision:
         closure = []
         subdiv_name_id_dict = {}  # Словарь {Имя_подразделения : id_подразделения} для поиска id по имени
 
-        # начинаем с 2 т.к. в бд уже есть подразделение с id = 1 - ROOT
-        id = 2
+
+        id = 2  # начинаем с 2 т.к. в бд уже есть подразделение с id = 1 - ROOT
         closure.append([1, 2])
+
+        self.log.info(f'File: {self.filepath_src}, Sheet: {self.sheet_name}')
 
         for index, row in self.df.iterrows():
             try:
                 # row.values[1] - строка вида: root_subdibision\subd1\subd2\subd3\subd4
                 # берем последнее и предпоследнее поле, получаме пару parent_name : child_name
-                parent_name = row.values[1].split('\\')[-2]
-                child_name = row.values[1].split('\\')[-1]
+                code = row['Код']
+                parent_name = row['Путь'].split('\\')[-2]
+                child_name = row['Путь'].split('\\')[-1]
+                path = row['Путь']
                 child_id = id
                 subdiv_name_id_dict[self.normalise_str(child_name)] = child_id
                 subdivision.append({
                     'id': child_id,
-                    'code': str(child_id) + '000',
+                    'code': int(code) * 10,     # todo: Убрать когда появятся нормальные коды
                     'name': child_name,
                     'parent_name': parent_name,
                     'parent_id': subdiv_name_id_dict[self.normalise_str(parent_name)]
@@ -53,7 +57,8 @@ class Subdivision:
                 closure.append([subdiv_name_id_dict[self.normalise_str(parent_name)], child_id])
                 id += 1
             except IndexError as e:
-                child_name = row.values[1].split('\\')[-1]
+                code = row['Код']
+                child_name = row['Путь'].split('\\')[-1]
 
                 # Если это первая строка файла, когда еще нет родителя - обрабатываем
                 if index == 0 and child_name:
@@ -61,7 +66,7 @@ class Subdivision:
                     self.log.debug('Except: Processing the first line')
                     subdivision.append({
                         'id': id,
-                        'code': 0,
+                        'code': int(code) * 10,  # todo: Убрать когда появятся нормальные коды
                         'name': child_name,
                         'parent_name': 'ROOT',
                         'parent_id': 1
